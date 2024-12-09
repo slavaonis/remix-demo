e87720fc7ecda32c6ddd7e6d67949f7da15487f2
Vyacheslav Onischenko <vyacheslav.onischenko@litslink.com>
2024-12-09 13:14:44 +0200

    fix: update types for useToggle

--- app/global/hooks/use-toggle.tsx
+++ app/global/hooks/use-toggle.tsx
@@ -1,15 +1,22 @@
-import {useState} from 'react';
+import {MouseEventHandler, useState} from 'react';
 
 //
 //
 
-export const useToggle = (defaultValue: boolean = false) => {
+export const useToggle = (
+  defaultValue: boolean = false,
+): {
+  value: boolean;
+  toggle: () => void;
+  setValue: (value: boolean) => void;
+  preventEventDefault: MouseEventHandler<HTMLButtonElement>;
+} => {
   const [value, setValue] = useState(defaultValue);
 
   return {
     value,
     toggle: () => setValue(v => !v),
     setValue: (val: boolean) => setValue(val),
-    preventEventDefault: (event: any) => event?.preventDefault?.(),
+    preventEventDefault: event => event?.preventDefault?.(),
   };
 };
